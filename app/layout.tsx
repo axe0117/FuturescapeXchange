8ebc44d73248fb227e26bf84871e0303090ec3a0
axe0117 <axe0117@gmail.com>
2026-03-01 09:04:18 -0500

    im committing it

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const montserrat = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: "Home | FutureXchange",
+  title: "Home | FutureXchange Summit",
   description: "US and Asia Innovation Summit"
 };
 
